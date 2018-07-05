d8f4e5aec4fd3e348cbc7bbabd084b48e965e8e2
Mike Upton <uptonm@wit.edu>
2018-07-05 16:26:29 -0400

    Init Commit - Visualize the Bitcoin Price Index

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Chart from './components/Chart';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Chart/>
+      </div>
+    );
+  }
+}
+
+export default App;
